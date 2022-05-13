9b5b00207629b09b8124aed87de23892a4382a23
Alex Cheung <alexc.sfu@gmail.com>
2022-05-13 00:34:11 +0000

    added index.js and breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,22 @@
 const request = require('request');
 
-let breed = process.argv.slice(2);
-
-const findBreed = (data) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${data}`, (err, resp, body) => {
-    if (err) {
-      //this only checks the connection error, not any other errors like 404 not found
-      console.log('Error', err);
-      console.log(resp.statusCode);
-      return;
+const fetchBreedDescription = (data, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${data}`, (error, resp, body) => {
+    if (error) {
+      return callback(error, null);
     }
+    
     const data = JSON.parse(body);
     let breedFound = data[0];
+    
     if (breedFound) {
-      console.log(breedFound.description);
+      return callback(null, breedFound.description);
     } else if (resp.statusCode === 404) {
-      console.log(`Page not found`);
+      return callback('404 Page not found', null);
     } else {
-      console.log(`Breed not found`);
+      return callback('Breed not found', null);
     }
   });
 };
 
-
-findBreed(breed);
+module.exports = {fetchBreedDescription};
